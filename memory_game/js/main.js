0957dd9ee7cc89741eef96c0b71f219efa4abf60
Katelyn <katelyncmiles@gmail.com>
2019-06-14 13:18:22 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+const cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+const cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
