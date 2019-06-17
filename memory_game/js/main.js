de4d4096bb7b1d48993807071ffb005a8e0291e1
Katelyn <katelyncmiles@gmail.com>
2019-06-16 20:21:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
-const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -15,8 +36,10 @@ if (cardsInPlay.length === 2) {
 }
 
 function flipCard(cardId) {
-console.log("User flipped" + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped" + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 checkForMatch();
 }
 
