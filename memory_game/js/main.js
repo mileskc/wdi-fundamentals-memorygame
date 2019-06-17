afb3602ac66dea72722c148052d782281605e167
Katelyn <katelyncmiles@gmail.com>
2019-06-16 23:34:22 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,6 +25,7 @@ const cards = [
 
 let cardsInPlay = [];
 
+
 function checkForMatch() {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -35,13 +36,25 @@ if (cardsInPlay.length === 2) {
 }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+const cardId = this.getAttribute('data-id');
 console.log("User flipped" + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+for (let i = 0; i < cards.length; i++) {
+	const cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+}
+
+createBoard();
